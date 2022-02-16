6a53e97ef1fbca2f6f4ed802d020fafe80fa13a2
Monnyann <37423861+Monnyann@users.noreply.github.com>
2022-02-16 16:07:13 +0100

    Introduction to DOM

---
+++ index.html.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <!-- Loop -->
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped></style>
---
+++ index2.html.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="tittle" id="test">Shittu Abimbola Muibat</h1>
+    <form action="" id="details">
+      <input type="text" value="Shittu Abimbola" placeholder="Name" />
+      <input type="email" value="monnyann10@gmail.con" placeholder="Email" />
+      <input type="password" value="adfghj67" placeholder="Password" />
+      <input type="number" value="08065607842" placeholder="Phone Number" />
+    </form>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+input {
+  padding: 0.9rem 1rem;
+  margin: 1rem;
+  display: flex;
+  flex-direction: column;
+  border-radius: 20px;
+  border: 1px solid indigo;
+  text-align: center;
+}
+</style>
